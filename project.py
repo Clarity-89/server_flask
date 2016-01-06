ece81a3df64acc8d879c769792d7e0cc6363ee5d
Clarity-89 <homes89@ukr.net>
2016-01-06 23:27:31 +0200

    Add API endpoint

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -11,6 +11,14 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
+# API endpoint
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+
+    return jsonify(MenuItems=[i.serialize for i in items])
 
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
